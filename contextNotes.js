cce1234e28895ab85756e96ad6e27a1f99a1da9b
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-08-15 10:37:56 -0400

    contextNotes

--- contextNotes.js
+++ contextNotes.js
@@ -1,4 +1,14 @@
 /*
   Some context on Context:
 
+    React data flows one way, top-down from parent to child via props. You can lift it via passing a method as a prop, but this gets very messy quickly.
+
+    Context is used to provide a global state - called a store. This state is going to be managed by the context itself. This is done by creating a Provider(which provides the state to its children), and a Consumer(which makes it available).
+
+    An optional step is to create Actions as well, but we won't need them in these applications.
+
+    The next step is the Reducer. This is the method that will keep track of your operations on the state and update it as necessary.
+
+
+
 */
